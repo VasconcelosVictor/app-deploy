6102532d033eebf6eab81fd5bf36b8de5b814414
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-04 14:59:17 -0400

    ajuste

--- trees_everywhere/build_files.sh
+++ trees_everywhere/build_files.sh
@@ -1,8 +1,8 @@
 
 echo " BUILD START"
-python3.9 -m virtualenv .env 
-source .env/bin/activate
-pip install -r requirements.txt
-python3.9 manage.py migrate
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate 
+
 # python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
