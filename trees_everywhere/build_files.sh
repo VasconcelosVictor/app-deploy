04df8a524956a5fe3c0b97c3dfe82329f173dab1
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-04 15:11:26 -0400

    ajuste

--- trees_everywhere/build_files.sh
+++ trees_everywhere/build_files.sh
@@ -1,13 +1,13 @@
 
 echo " BUILD START"
 # Atualiza o pip
-python3.9 -m ensurepip
+python3.10 -m pip install --upgrade pip
 
 # Instala as dependências Python
 pip install -r requirements.txt
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate 
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py makemigrations
+python3.10 manage.py migrate 
 
 # python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
