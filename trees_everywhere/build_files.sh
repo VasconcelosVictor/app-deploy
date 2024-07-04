f0b92244f60a8b093c736f43c63466826dc2b4cc
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-04 15:18:12 -0400

    mais uma correcao

--- trees_everywhere/build_files.sh
+++ trees_everywhere/build_files.sh
@@ -1,13 +1,23 @@
 
 echo " BUILD START"
 # Atualiza o pip
-python3.9 -m pip install --upgrade pip
+# Atualiza o pip
+/usr/bin/python3.9 -m ensurepip
+/usr/bin/python3.9 -m pip install --upgrade pip
 
-# Instala as dependências Python
-pip install -r requirements.txt
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate 
+# Cria e ativa o ambiente virtual (opcional, se necessário)
+# /usr/bin/python3.9 -m venv myenv
+# source myenv/bin/activate
+
+# Instala as dependências do projeto
+/usr/bin/python3.9 -m pip install -r requirements.txt
+
+# Executa as migrações do Django
+/usr/bin/python3.9 manage.py migrate
+
+# Coleta os arquivos estáticos
+/usr/bin/python3.9 manage.py collectstatic --noinput --clear
+
 
 # python3.9 manage.py collectstatic  --noinput --clear
 echo " BUILD END"
