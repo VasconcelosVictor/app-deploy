5deb038aade9b81eb02dad23d87da50da395665d
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-04 14:05:00 -0400

    tentativa de deploy

--- trees_everywhere/trees_everywhere/urls.py
+++ trees_everywhere/trees_everywhere/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", user_login, name='login'),
-    path('', include('core.urls')),
+    # path("", user_login, name='login'),
+    # path('', include('core.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
