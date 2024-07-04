8f0838fba90afc7b25816c6831abb2633c83aaf2
VasconcelosVictor <victorvasconcelos6x@hotmail.com>
2024-07-04 15:40:37 -0400

    mudando o banco pra ve se vai

--- trees_everywhere/trees_everywhere/settings.py
+++ trees_everywhere/trees_everywhere/settings.py
@@ -87,15 +87,9 @@ WSGI_APPLICATION = 'trees_everywhere.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'testando',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
-
 }
 
 
